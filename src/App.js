644fc930462095d38e20d4f7823052656786d929
kimchirice <kimchi@u12.co>
2020-11-12 16:56:13 +1100

    [Add] install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" id="main">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          it's not about cycling
         </p>
      
       </header>
